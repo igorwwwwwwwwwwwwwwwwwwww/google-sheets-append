265430b3db7a2eeba884defaa95632388b9ac909
igor <igor@igor.io>
2020-04-22 15:22:09 +0200

    reorg-wochen

--- main.go
+++ main.go
@@ -2,85 +2,27 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 
-	"golang.org/x/oauth2"
+	"github.com/igorwwwwwwwwwwwwwwwwwwww/google-sheets-append/auth"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
 )
 
 // https://developers.google.com/sheets/api/quickstart/go
 
-var spreadsheetId = flag.String("spreadsheet", "", "")
 var range_ = flag.String("range", "A1", "")
 var verbose = flag.Bool("v", false, "")
-
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
 
 func main() {
 	flag.Parse()
 
-	if *spreadsheetId == "" {
-		fmt.Println("usage: google-sheets-append <options> <values...>")
+	if len(flag.Args()) == 0 {
+		fmt.Println("usage: google-sheets-append <options> <spreadsheet-id> <values...>")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -97,15 +39,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	client := getClient(config)
+
+	client := auth.GetClient(config)
 
 	srv, err := sheets.New(client)
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
+	spreadsheetId := flag.Arg(0)
+
 	// https://golang.org/doc/faq#convert_slice_of_interface
-	t := flag.Args()
+	t := flag.Args()[1:]
 	s := make([]interface{}, len(t))
 	for i, v := range t {
 		s[i] = v
@@ -122,7 +67,7 @@ func main() {
 	// "USER_ENTERED"
 	valueInputOption := "USER_ENTERED"
 
-	resp, err := srv.Spreadsheets.Values.Append(*spreadsheetId, *range_, valuerange).ValueInputOption(valueInputOption).Context(ctx).Do()
+	resp, err := srv.Spreadsheets.Values.Append(spreadsheetId, *range_, valuerange).ValueInputOption(valueInputOption).Context(ctx).Do()
 	if err != nil {
 		log.Fatal(err)
 	}
